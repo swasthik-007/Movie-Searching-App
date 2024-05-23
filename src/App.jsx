6a5ea66b0ea46d27c6e4e94d83ce60c73d6a7749
swashik-007 <mohanty.swastik4@gmail.com>
2024-05-23 23:47:18 +0530

    added debounce

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from './components/Navbar/Navbar';
 import MainRoutes from './routes/Mainroutes';
 
 function App() {
-  console.log(import.meta.env); 
   return (
     <>
       <Navbar />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,14 @@
-import { useEffect, useState } from "react";
-import { searchMovie } from '../apis/omdb';
-import axios from "axios";
 // Components import
 import MovieCard from "../components/MovieCard/MovieCard";
+
 // CSS imports
 import './Home.css';
-function Home() {
-    const [movieList, setMovieList] = useState([]);
-    async function downloadDefaultMovies(...args) {
-        const urls = args.map((name) => searchMovie(name));
-        console.log(urls);
-        const response = await axios.all(urls.map(url => axios.get(url)));
-        const movies = response.map((movieResponse) => movieResponse.data.Search);
-        setMovieList([].concat(...movies));
-    }
+import useMovieList from "../hooks/useMovieList";
 
-    useEffect(() => {
-        downloadDefaultMovies('harry', 'avengers', 'batman');
-    }, []);
+function Home() {
+
+    const {movieList} = useMovieList('harry', 'avengers', 'batman');
+
     return (
         <>
             <div className="movie-card-wrapper">
