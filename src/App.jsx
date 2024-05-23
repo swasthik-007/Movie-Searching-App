13a4e2d6c844aa45e6c20bd01c014c1aa10684ca
swashik-007 <mohanty.swastik4@gmail.com>
2024-05-23 19:56:04 +0530

    parallel movie download done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar/Navbar';
 import MainRoutes from './routes/Mainroutes';
 
 function App() {
-  console.log(import.meta.env.VITE_API_KEY); 
+  console.log(import.meta.env); 
   return (
     <>
       <Navbar />
