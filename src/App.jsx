638706c3f2782a6cd4f6cd8bbd4ecbbe0bd67a2d
swashik-007 <mohanty.swastik4@gmail.com>
2024-05-24 17:41:05 +0530

    added persistence

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 // CSS import
 import './App.css'
-
 // Component imports
 import Navbar from './components/Navbar/Navbar';
 import MainRoutes from './routes/Mainroutes';
 
 // Context import
 import ThemeContext from './context/ThemeContext';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
   const [theme, setTheme] = useState('dark');
+
+  useEffect(() => {
+    const userTheme = localStorage.getItem('app-theme');
+    if(userTheme != null) {
+      setTheme(userTheme);
+    }
+  }, []);
 
   return (
     <>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,14 @@
 import { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-
 // CSS import
 import './Navbar.css';
-
 // Context import
 import ThemeContext from '../../context/ThemeContext';
-
 // Component import
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import useMovieList from '../../hooks/useMovieList';
 import useDebounce from '../../hooks/useDebounce';
-
 function Navbar() {
     // const resultListRef = useRef(null);
     const [isAutoCompleteVisible, setIsAutoCompleteVisible] = useState(false);
@@ -21,6 +17,16 @@ function Navbar() {
     const navigator = useNavigate();
 
     const {theme, setTheme} = useContext(ThemeContext);
+
+    function updateTheme() {
+        if(theme == 'dark') {
+            setTheme('light');
+            localStorage.setItem('app-theme', 'light');
+        } else {
+            setTheme('dark');
+            localStorage.setItem('app-theme', 'dark');
+        }
+    }
 
     function handleAutoCompleteClick(e, movieImdbId) {
         navigator(`/movie/${movieImdbId}`);
@@ -57,7 +63,7 @@ function Navbar() {
                     ))}
                 </div>
             </div>
-            <div onClick={() => setTheme((theme == 'dark') ? 'light' : 'dark')}>
+            <div onClick={updateTheme}>
                 <FontAwesomeIcon className='theme-icon' icon={(theme == 'dark') ? faSun : faMoon} />
             </div>
         </div>
