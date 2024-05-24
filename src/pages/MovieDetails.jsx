018af4024e30c65c5d89e73f51744e9719c3c61c
swashik-007 <mohanty.swastik4@gmail.com>
2024-05-24 17:34:41 +0530

    added dark theme css

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -3,6 +3,13 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { searchMovieById } from "../apis/omdb";
 import MovieCard from "../components/MovieCard/MovieCard";
+import { Rating } from '@smastrom/react-rating'
+
+
+// CSS imports
+import './MovieDetails.css';
+import '@smastrom/react-rating/style.css'
+
 
 function MovieDetails() {
     const [movie, setMovie] = useState(null);
@@ -10,24 +17,40 @@ function MovieDetails() {
 
     async function downloadMovie(){
         const response = await axios.get(searchMovieById(id));
-        console.log(response.data);
         setMovie(response.data);
     }
-
     useEffect(() => {
         downloadMovie();
-    }, []);
+    }, [id]);
 
     return (
-        <>
+        <div className="movie-details-wrapper">
             {movie && <MovieCard 
                         Title={movie.Title} 
                         Year={movie.Year}
                         Type={movie.Type}
                         Poster={movie.Poster} 
+                        id={movie.imdbID}
                         />
             }
-        </>
+            {movie && <div className="movie-details">
+                <div>
+                    Plot: {movie.Plot}
+                </div>
+                <div>
+                    Actors: {movie.Actors}
+                </div>
+                <div>
+                    Genre: {movie.Genre.split(',').map((genre) => {
+                        return <span className="genre" key={genre}>{genre}</span>
+                    })}
+                </div>
+                <div>
+                    <Rating items={10} value={Math.floor(movie.imdbRating)} />
+                </div>
+            </div>}
+        </div>
     )
 }
+
 export default MovieDetails;
